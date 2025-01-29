61a903cbbece116c86dcae786fb82575774f6b2c
Pedro Sanches <sanchesmil@gmail.com>
2025-01-28 21:05:29 -0300

    exercicios 1 a 20 resolvidos

---
+++ exercicios.py
@@ -0,0 +1,124 @@
+# #### Inteiros (`int`)
+
+# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+#num1 = int(input('Digite o primeiro número:'))
+#num2 = int(input('Digite o segundo número: '))
+#print(f"Somatório: {num1 + num2}")
+
+# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+#num = float(input('Digite um número: '))
+#print(f"Resto da divisão por 5: {num % 5}")
+
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+#num1 = float(input('Digite o primeiro número: '))
+#num2 = float(input('Digite o segundo número: '))
+#print(f"Multiplicação: {num1 * num2}")
+
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+#num1 = float(input('Digite o primeiro número: '))
+#num2 = float(input('Digite o segundo número: '))
+#print(f"Divisão inteira: {num1 // num2}")
+
+# 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+
+#num = float(input('Digite um número: '))
+#print(f"Quadrado do número: {num ** 2}")
+
+# #### Números de Ponto Flutuante (`float`)
+
+# 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
+
+# num1 = float(input('Digite o primeiro número: '))
+# num2 = float(input('Digite o segundo número: '))
+# print(f"Adição: {num1 + num2}")
+
+# 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+
+# num1 = float(input('Digite o primeiro número: '))
+# num2 = float(input('Digite o segundo número: '))
+# print(f"Média: {(num1 + num2) / 2}")
+
+# 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+
+# base = float(input('Digite a base: '))
+# expoente = float(input('Digite o expoente: '))
+# print(f"Potência de {base} e expoente {expoente} é {base ** expoente}")
+
+# 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+
+# temp_celsius = float(input('Informe a temperatura em graus Celsius: '))
+# temp_fahrenheit = (temp_celsius * 9/5) + 32
+# print(f"Graus Fahrenheit: {temp_fahrenheit}")
+
+# 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+
+# raio = float(input('Digite o raio do círculo: '))
+# area_circulo = 3.14 * raio**2
+# print(f"Área do círculo: {area_circulo}")
+
+# #### Strings (`str`)
+
+# 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+
+# entrada = input('Digite uma palavra: ')
+# print(entrada.upper())
+
+# 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+
+# nome_completo = input('Digite seu nome completo: ')
+# print(nome_completo.lower())
+
+# 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+
+# frase = input('Digite um frase: ')
+# print(frase.strip())
+
+# 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+
+# data = input('Digite uma data no formato dd/mm/aaaa:')
+# print(data.split('/'))
+
+# 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
+# palavra1 = input('Digite uma palavra: ')
+# palavra2 = input('Digite outra palavra:')
+# print(palavra1 + palavra2)
+
+# #### Booleanos (`bool`)
+
+# 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+
+# expressao1 = bool(int(input('Você é homem? 1 (SIM) - 0 (NÃO) ')))
+# expressao2 = bool(int(input('Você é mulher? 1 (SIM) - 0 (NÃO) ')))
+
+# print(f"Operador AND: {expressao1} AND {expressao2} = {expressao1 and expressao2}")
+
+# 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+
+# expressao1 = bool(int(input('Você é homem? 1 (SIM) - 0 (NÃO) ')))
+# expressao2 = bool(int(input('Você é mulher? 1 (SIM) - 0 (NÃO) ')))
+# print(f"Operador AND: {expressao1} OR {expressao2} = {expressao1 or expressao2}")
+
+# 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+
+# entrada = bool(int(input('Você é músico? 1 (SIM) - 0 (NÃO) ')))
+# print(not entrada)
+
+# 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+
+# num1 = int(input('Digite o primeiro número: '))
+# num2 = int(input('Digite o segundo número: '))
+# sao_iguais = num1 == num2
+# print(f"Números {num1} e {num2} {'são iguais' if sao_iguais else 'não são iguais'}")
+
+# #### try-except e if
+
+# 21: Conversor de Temperatura
+# 22: Verificador de Palíndromo
+# 23: Calculadora Simples
+# 24: Classificador de Números
+# 25: Conversão de Tipo com Validação
